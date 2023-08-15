1eed27d1d6e1c12ead88a52e4821bfd9cedfe916
unyime <unyimeusen01@gmail.com>
2023-08-15 22:16:41 +0100

    create nav and header basic setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import PopUp from "./components/PopUp";
+
 const App = () => {
   return (
     <div className="app">
-      <h1>heyyy</h1>
+      <Nav />
+      <Header />
+      <Feed />
+      <PopUp />
     </div>
   );
 };
