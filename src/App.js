2b59dfe257f60724ac0fd2ec306cb15e74c2317e
unyime <unyimeusen01@gmail.com>
2023-08-18 07:16:37 +0100

    fetch user data and link instagram icon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
+import { useState, useEffect } from "react";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Feed from "./components/Feed";
 import PopUp from "./components/PopUp";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  const userId = "4984e9b8-1364-4bb6-b6cf-d4cbcf9994e6";
+
+  const getUser = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/users?user_uuid=${userId}`
+      );
+      const data = await response.json();
+      setUser(data[0]); //to get the object in the array
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
-    <div className="app">
-      <Nav />
-      <Header />
-      <Feed />
-      {/* <PopUp /> */}
-    </div>
+    <>
+      {user && (
+        <div className="app">
+          <Nav url={user.instagram_url} />
+          <Header />
+          <Feed />
+          {/* <PopUp /> */}
+        </div>
+      )}
+    </>
   );
 };
 
